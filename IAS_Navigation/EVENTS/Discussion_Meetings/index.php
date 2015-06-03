9842b94aaa3f1a194c3b307acc63e78b8a3db3f1
Shruthisdst <shruthitr.nayak@gmail.com>
2015-06-03 16:47:28 +0530

    Commit No. 43 - Discussion meetings xml added

---
+++ IAS_Navigation/EVENTS/Discussion_Meetings/index.php
@@ -0,0 +1,4 @@
+<h1>ACADEMY DISCUSSION MEETINGS</h1>
+<h2>&nbsp;</h2>
+
+<p>Following Discussion Meetings were held so far under this programme in different locations involving the cross-section of the scientists from India and abroad</p>
